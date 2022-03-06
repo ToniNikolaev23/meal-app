f4f07e1c7d3209bd5e223ffc05a6b3b8b6629a70
Toni Stoyanov <toni.nikolaev.23@gmail.com>
2022-03-06 16:49:46 +0200

    Make some fixes and add Favorite button to the right in Header

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
 
 import { MEALS } from "../data/dummy-data";
 
@@ -26,6 +28,17 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 
   return {
     headerTitle: selectedMeal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("Mark as Favorite!");
+          }}
+        />
+      </HeaderButtons>
+    ),
   };
 };
 
